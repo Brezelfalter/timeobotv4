13f4ad69963afb2e8723dc2be211c0d65ab7f765
LUKAS U IDIOT <lukas.neumanntuf@gmail.com>
2022-06-19 00:22:38 +0200

    lol

--- main.py
+++ main.py
@@ -25,8 +25,10 @@ async def lostFunc(client):
     '''
     for filename in os.listdir(f"{os.path.abspath('.')}/cogs"):
         if filename.endswith(".py"):
-            await client.load_extension(f"cogs.{filename[:-3]}")
-
+            try:
+                await client.load_extension(f"cogs.{filename[:-3]}")
+            except:
+                pass
 
 
 
